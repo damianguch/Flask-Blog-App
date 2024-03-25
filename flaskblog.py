b72c47a83f63e6fead88f30887068d9c4ed7a7f0
Damian Oguche <damianguch@yahoo.com>
2024-03-25 15:07:06 +0000

    Modified the flaskblog.py file and the templates files to implement template inheritance

---
+++ flaskblog.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Damian Oguche',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 25, 2024'
+    },
+
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 29, 2024'
+    }
+]
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
